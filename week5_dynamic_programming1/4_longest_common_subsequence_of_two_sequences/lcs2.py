d85458b690d20628fa71cd7bd7c8414952d3101a
Shiv2195 <35445257+Shiv2195@users.noreply.github.com>
2019-01-29 10:51:58 +0530

    Update lcs2.py

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,22 +1,22 @@
-#Uses python3
-
-import sys
-
+# Uses python3
 def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+    m = len(a)
+    n = len(b)
+    L = [[0]*(n+1) for i in range(m+1)]
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if a[i-1] == b[j-1]:
+                L[i][j] = L[i-1][j-1] + 1
+            else:
+                L[i][j] = max(L[i-1][j], L[i][j-1])
 
-    n = data[0]
-    data = data[1:]
-    a = data[:n]
+    return L[m][n]
 
-    data = data[n:]
-    m = data[0]
-    data = data[1:]
-    b = data[:m]
 
-    print(lcs2(a, b))
+m = int(input())
+a = list(map(int, input().split()))
+n = int(input())
+b = list(map(int, input().split()))
+print(lcs2(a, b))
+
