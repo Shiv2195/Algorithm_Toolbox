5c19b482bd6b54a32e3246a2f8688333ecf4ebdb
Shiv2195 <35445257+Shiv2195@users.noreply.github.com>
2019-01-29 10:53:14 +0530

    Update lcs3.py

--- week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -1,23 +1,37 @@
-#Uses python3
+# Uses python3
 
-import sys
+'''
+With Repetitions allowed
+def optimal_weight(W, w):
+    value = [0] * W
+    for i in range(1, W):
+       for j in w:
+            if j <= i:
+                val = value[i-j]+j
+                if val > value[i]:
+                    value[i]=val
+    print(value)
+    return value[W-1]
+'''
+def optimal_weight(W,w):
+    m = len(w)
 
-def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+    value = [[0]*(m+1) for i in range(W+1)]
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    an = data[0]
-    data = data[1:]
-    a = data[:an]
-    data = data[an:]
-    bn = data[0]
-    data = data[1:]
-    b = data[:bn]
-    data = data[bn:]
-    cn = data[0]
-    data = data[1:]
-    c = data[:cn]
-    print(lcs3(a, b, c))
+    for i in range(1,m+1):
+        for j in range(1,W+1):
+            value[j][i] = value[j][i-1]
+            if w[i-1] <= j:
+                val = value[j-w[i-1]][i-1]+w[i-1]
+                if value[j][i] < val:
+                    value[j][i] = val
+    #print(value)
+    return value[W][m]
+
+
+
+
+
+W,n = map(int,input().split())
+w = list(map(int,input().split()))
+print(optimal_weight(W,w))
